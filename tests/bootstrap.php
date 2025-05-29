14163622a7a181fc474c76b2e62f1611b7b7e0a1
John Boehr <john@johnboehr.is>
2025-05-29 16:33:55 -0700

    Make sure to use phpstan's phpparser in phpunit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,15 +17,9 @@
  */
 declare(strict_types=1);
 
-if (
-    version_compare(\Composer\InstalledVersions::getVersion('laravel/framework') ?? '', '10', '>=') &&
-    version_compare(\Composer\InstalledVersions::getVersion('laravel/framework') ?? '', '11', '<') &&
-    version_compare(\Composer\InstalledVersions::getVersion('nikic/php-parser') ?? '', '5', '>=')
-) {
-    // some weird conflict with PHPStan/PhpParser
-    /** @var \Composer\Autoload\ClassLoader $loader */
-    $loader = require __DIR__ . '/../vendor/autoload.php';
-    $loader->setPsr4('PhpParser\\', [
-        __DIR__ . '/../vendor/phpstan/phpstan/phpstan.phar/vendor/nikic/php-parser/src'
-    ]);
-}
+// Make sure to use phpstan's phpparser otherwise there are weird class conflicts
+/** @var \Composer\Autoload\ClassLoader $loader */
+$loader = require __DIR__ . '/../vendor/autoload.php';
+$loader->setPsr4('PhpParser\\', [
+    __DIR__ . '/../vendor/phpstan/phpstan/phpstan.phar/vendor/nikic/php-parser/src'
+]);
